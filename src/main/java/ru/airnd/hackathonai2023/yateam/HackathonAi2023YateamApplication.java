160107285a6a75ac53aac36ce4c85d424b40af17
Grig Alex <alexengrigdev@gmail.com>
2023-05-04 14:20:17 +0300

    Init project

---
+++ src/main/java/ru/airnd/hackathonai2023/yateam/HackathonAi2023YateamApplication.java
@@ -0,0 +1,13 @@
+package ru.airnd.hackathonai2023.yateam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonAi2023YateamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonAi2023YateamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/airnd/hackathonai2023/yateam/HackathonAi2023YateamApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.airnd.hackathonai2023.yateam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonAi2023YateamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
