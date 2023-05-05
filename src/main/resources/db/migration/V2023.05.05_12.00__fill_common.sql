2a8178f53d2c59fb0bfe90e55a5c2f1514181eae
Grig Alex <alexengrigdev@gmail.com>
2023-05-05 14:52:30 +0300

    Add users

--- src/main/resources/db/migration/V2023.05.05_12.00__fill_common.sql
+++ src/main/resources/db/migration/V2023.05.05_12.00__fill_common.sql
@@ -1,5 +1,11 @@
 INSERT INTO "user" (id, username, password)
-VALUES (0, 'admin', 'admin');
+VALUES (0, 'admin', 'admin'),
+       (1, 'user', 'user'),
+       (2, 'user0', 'user0'),
+       (3, 'user1', 'user1'),
+       (4, 'user2', 'user2'),
+       (5, 'user3', 'user3'),
+       (6, 'user4', 'user4');
 
 INSERT INTO category (id, name, description)
 VALUES (1, 'Анализ', 'Практики по анализу'),
