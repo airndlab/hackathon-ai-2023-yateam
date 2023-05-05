85fba17cd124995d87b2315fcc33aca44690b7dc
Grig Alex <alexengrigdev@gmail.com>
2023-05-05 12:28:12 +0300

    Reset controllers

--- src/main/java/ru/airnd/hackathonai2023/yateam/controller/CommentController.java
+++ src/main/java/ru/airnd/hackathonai2023/yateam/controller/CommentController.java
@@ -1,5 +1,7 @@
 package ru.airnd.hackathonai2023.yateam.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,19 +9,20 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.airnd.hackathonai2023.yateam.service.CommentService;
 
 @RestController
 @RequestMapping("/api/comments")
-//@Tag(name = "CommentController", description = "Контроллер для работы с комментариями")
+@Tag(name = "CommentController", description = "Контроллер для работы с комментариями")
 @RequiredArgsConstructor
 public class CommentController {
 
-//    private final CommentService commentService;
+    private final CommentService commentService;
 
     @DeleteMapping("/{commentId}")
-//    @Operation(summary = "Удаление комментария")
+    @Operation(summary = "Удаление комментария")
     public ResponseEntity<Void> deleteComment(@PathVariable Integer commentId) {
-//        commentService.deleteComment(commentId);
+        commentService.deleteComment(commentId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
--- src/main/java/ru/airnd/hackathonai2023/yateam/controller/PracticeController.java
+++ src/main/java/ru/airnd/hackathonai2023/yateam/controller/PracticeController.java
@@ -1,79 +1,87 @@
 package ru.airnd.hackathonai2023.yateam.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.airnd.hackathonai2023.yateam.dto.CommentDTO;
 import ru.airnd.hackathonai2023.yateam.dto.CreateCommentDTO;
 import ru.airnd.hackathonai2023.yateam.dto.DetailedPracticeDTO;
 import ru.airnd.hackathonai2023.yateam.dto.PracticeDTO;
+import ru.airnd.hackathonai2023.yateam.service.CommentService;
+import ru.airnd.hackathonai2023.yateam.service.PracticeService;
+import ru.airnd.hackathonai2023.yateam.service.RatingService;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/practices")
-//@Tag(name = "PracticeController", description = "Контроллер для работы с практиками")
+@Tag(name = "PracticeController", description = "Контроллер для работы с практиками")
 @RequiredArgsConstructor
 public class PracticeController {
 
-//    private final PracticeService practiceService;
-//    private final RatingService ratingService;
-//    private final CommentService commentService;
+    private final PracticeService practiceService;
+    private final RatingService ratingService;
+    private final CommentService commentService;
 
     @GetMapping
-//    @Operation(summary = "Получить список всех практик")
+    @Operation(summary = "Получить список всех практик")
     public ResponseEntity<List<PracticeDTO>> getAllPractices() {
-        return ResponseEntity.ok(List.of(
-                PracticeDTO.builder().id(1).author("author1").category("Category1").name("name1").rating(1.0).votes(3)
-                        .build()
-        ));
+        return ResponseEntity.ok(practiceService.getAllPractices());
     }
 
     @GetMapping("/{practiceId}")
-//    @Operation(summary = "Получить информацию о практике")
+    @Operation(summary = "Получить информацию о практике")
     public ResponseEntity<DetailedPracticeDTO> getPracticeById(
             @PathVariable Integer practiceId
     ) {
-        return ResponseEntity.ok(
-                DetailedPracticeDTO.builder().author("author1").category("Category1").description("desc").votes(3)
-                        .rating(1.0).link("http://localhost").build());
+        return ResponseEntity.ok(practiceService.getPracticeById(practiceId));
     }
 
     @PostMapping("/{practiceId}/ratings")
-//    @Operation(summary = "Добавить оценку практике")
+    @Operation(summary = "Добавить оценку практике")
     public ResponseEntity<Void> addRating(
             @PathVariable Integer practiceId,
             @RequestParam Integer rating
     ) {
-//        ratingService.addRating(practiceId, rating);
+        ratingService.addRating(practiceId, rating);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @DeleteMapping("/{practiceId}/ratings")
-//    @Operation(summary = "Отменить оценку практики")
+    @Operation(summary = "Отменить оценку практики")
     public ResponseEntity<Void> deleteRating(
             @PathVariable Integer practiceId
     ) {
-//        ratingService.deleteRating(practiceId);
+        ratingService.deleteRating(practiceId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @GetMapping("/{practiceId}/comments")
-//    @Operation(summary = "Получить список комментариев для практики")
+    @Operation(summary = "Получить список комментариев для практики")
     public ResponseEntity<List<CommentDTO>> getCommentsForPractice(
             @PathVariable Integer practiceId
     ) {
-        return ResponseEntity.ok(List.of(CommentDTO.builder().comment("comment").id(1).username("username").build()));
+        return ResponseEntity.ok(commentService.getCommentsForPractice(practiceId));
     }
 
     @PostMapping("/{practiceId}/comments")
-//    @Operation(summary = "Создать новый комментарий к практике")
+    @Operation(summary = "Создать новый комментарий к практике")
     public ResponseEntity<Void> addCommentToPractice(
             @PathVariable Integer practiceId,
             @RequestBody @Validated CreateCommentDTO createCommentDTO
     ) {
+        commentService.addCommentToPractice(practiceId, createCommentDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
