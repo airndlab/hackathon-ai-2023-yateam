71a1191b66ed334eef1056f40e8e25ef0981cf27
a.kurilova <a.kurilova>
2023-05-05 16:22:23 +0300

    fix styles

--- src/main/webapp/src/components/practice-page/styles/PracticePageStyles.js
+++ src/main/webapp/src/components/practice-page/styles/PracticePageStyles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Background = styled.div`
   width: 100%;
   background-color: rgba(32, 33, 35, 1);
-  min-height: 100vh;
+  min-height: calc(100vh - 48px);
 `;
 
 export const Container = styled.div`
