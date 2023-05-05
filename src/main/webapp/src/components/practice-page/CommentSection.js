753bcddf9ce2e22284ccba7b2ef61c82898632a8
Andrey Mironov <mironov.a@iac.spb.ru>
2023-05-05 16:52:06 +0700

    add comment section

--- src/main/webapp/src/components/practice-page/CommentSection.js
+++ src/main/webapp/src/components/practice-page/CommentSection.js
@@ -1,61 +1,83 @@
-import { useState } from "react";
+import { useState, useContext, useEffect } from 'react';
+import { UserContext } from "../../contexts/UserContext";
+import { postComment } from "../../api";
 
-const CommentSection = ({ comments, onAddComment, onDeleteComment }) => {
-  const [newComment, setNewComment] = useState("");
+function CommentSection({ practiceId }) {
+  const [commentText, setCommentText] = useState("");
+  const [comments, setComments] = useState([]);
+  const { user } = useContext(UserContext);
 
-  const handleAddComment = (e) => {
-    e.preventDefault();
-    if (!newComment) return;
-    onAddComment(newComment);
-    setNewComment("");
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (commentText.trim() === "") {
+      return;
+    }
+    try {
+      const newComment = await postComment(practiceId, commentText);
+      setComments([...comments, newComment]);
+      setCommentText("");
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  const handleDeleteComment = (id) => {
-    onDeleteComment(id);
+  const handleCommentTextChange = (event) => {
+    setCommentText(event.target.value);
   };
+
+  useEffect(() => {
+    async function fetchComments() {
+      try {
+        const commentsData = await fetch(`/api/practices/${practiceId}/comments`).then((res) => res.json());
+        setComments(commentsData);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchComments();
+  }, [practiceId]);
 
   return (
-      <div>
-        <h2 className="text-lg font-bold mb-2">Comments</h2>
-        {comments?.length > 0 ? (
-            <ul className="space-y-4">
-              {comments.map((comment) => (
-                  <li key={comment.id} className="border rounded-lg p-4">
-                    <p className="text-gray-700">{comment.text}</p>
-                    <p className="text-sm text-gray-500 mt-2">
-                      <span>Posted by {comment.author}</span>{" "}
-                      {comment.deletable && (
-                          <button
-                              className="text-red-500 hover:text-red-700 ml-2"
-                              onClick={() => handleDeleteComment(comment.id)}
-                          >
-                            Delete
-                          </button>
-                      )}
-                    </p>
+      <div className="bg-gray-100 rounded-lg p-4">
+        <h2 className="font-bold mb-4">Comments</h2>
+        {user ? (
+            <form onSubmit={handleSubmit} className="mb-4">
+          <textarea
+              className="w-full border rounded-lg py-2 px-3"
+              placeholder="Add a comment..."
+              value={commentText}
+              onChange={handleCommentTextChange}
+          />
+              <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg mt-2">
+                Add Comment
+              </button>
+            </form>
+        ) : (
+            <p className="text-center">
+              <a className="text-blue-500 hover:underline" href="/login">Log in</a> to add comments.
+            </p>
+        )}
+        {comments?.length === 0 ? (
+            <p>No comments yet.</p>
+        ) : (
+            <ul>
+              {comments?.map((comment) => (
+                  <li key={comment.id} className="mb-4">
+                    <div className="flex items-center mb-2">
+                      <img
+                          className="w-8 h-8 rounded-full mr-2"
+                          src={`https://ui-avatars.com/api/?name=${encodeURI(comment.username)}`}
+                          alt={comment.username}
+                      />
+                      <p className="font-bold">{comment.username}</p>
+                    </div>
+                    <p>{comment.comment}</p>
                   </li>
               ))}
             </ul>
-        ) : (
-            <p className="text-gray-500">No comments yet.</p>
         )}
-        <form onSubmit={handleAddComment} className="mt-4">
-          <label htmlFor="newComment" className="block font-medium mb-1">
-            Add a comment
-          </label>
-          <textarea
-              id="newComment"
-              name="newComment"
-              className="border rounded-lg p-2 w-full mb-2"
-              value={newComment}
-              onChange={(e) => setNewComment(e.target.value)}
-          />
-          <button className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700">
-            Add
-          </button>
-        </form>
       </div>
   );
-};
+}
 
 export default CommentSection;
