540ddc6330dd0b6649b677e18ed6534b8ace1409
Grig Alex <alexengrigdev@gmail.com>
2023-07-29 18:57:29 +0300

    Add my vote

--- src/main/webapp/src/components/practice-page/PracticePage.js
+++ src/main/webapp/src/components/practice-page/PracticePage.js
@@ -5,9 +5,16 @@ import CommentSection from './CommentSection';
 import { Background, Container, FlexRow, FlexRowLabel, RatingContainer, Title } from './styles/PracticePageStyles';
 import StarRating from './StarRating';
 import { isNil } from 'lodash';
-import { Link } from '../common/LinkStyle';
+import {Link, RouteLink} from '../common/LinkStyle';
 import { useSelector } from 'react-redux';
 import Header from '../common/Header';
+
+const getVotesWord = (votes = 0) => {
+  const target = votes.toString()
+  if (target.endsWith('1')) return 'голос'
+  if (target.endsWith('2') || target.endsWith('3') || target.endsWith('4')) return 'голоса'
+  return 'голосов'
+}
 
 const PracticePage = () => {
   const { id } = useParams();
@@ -83,12 +90,22 @@ const PracticePage = () => {
                   <FlexRowLabel>Рейтинг:</FlexRowLabel>
                   <RatingContainer>
                     <p className="mr-2">{practice?.rating?.toFixed?.(1)}</p>
-                    <p>({practice?.votes} голосов)</p>
-                    <StarRating
-                        onRate={handleVote} onRemoveVote={handleCancelVote}
-                        userCanVote={!isNil(user) && isNil(vote)}
-                        userHasVoted={userHasVoted} rating={vote ?? 0} practiceId={id} />
+                    <p>({practice?.votes} {getVotesWord(practice?.votes)})</p>
                   </RatingContainer>
+                </FlexRow>
+                <FlexRow>
+                  <FlexRowLabel>Моя оценка:</FlexRowLabel>
+                  {user ? (
+                    <StarRating
+                      onRate={handleVote} onRemoveVote={handleCancelVote}
+                      userCanVote={!isNil(user) && isNil(vote)}
+                      userHasVoted={userHasVoted} rating={vote ?? 0} practiceId={id}
+                    />
+                  ) : (
+                    <span>
+                      <RouteLink to="/loginreact">Войдите</RouteLink>, чтобы оставить свой голос
+                    </span>
+                  )}
                 </FlexRow>
                 <CommentSection practiceId={id} />
               </>
