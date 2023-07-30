ec8e24a33db6092dcae9c9d6499c6371e7d680b0
Grig Alex <alexengrigdev@gmail.com>
2023-07-30 18:53:58 +0300

    Add redirect to login

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,15 +1,16 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 import MainPage from './components/main-page/MainPage';
 import PracticePage from './components/practice-page/PracticePage';
 import LoginPage from './components/login-page/LoginPage';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchCategories, fetchUser } from './redux/actions';
 
 function App() {
   const dispatch = useDispatch();
+  const user = useSelector(reducer => reducer?.user);
 
   useEffect(() => {
     fetchCategories(dispatch);
@@ -20,8 +21,12 @@ function App() {
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Header />
         <Switch>
-          <Route exact path="/" component={MainPage} />
-          <Route exact path="/practices/:id" component={PracticePage} />
+          <Route exact path="/">
+            {user ? <MainPage /> : <Redirect to="/loginreact"/>}
+          </Route>
+          <Route exact path="/practices/:id">
+            {user ? <PracticePage /> : <Redirect to="/loginreact"/>}
+          </Route>
           <Route exact path="/loginreact" component={LoginPage} />
         </Switch>
         <Footer />
