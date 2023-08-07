d03f4904d264d8c93f41983c512c6dbfb92678e3
Grig Alex <alexengrigdev@gmail.com>
2023-08-07 11:57:03 +0300

    Update footer

--- src/main/webapp/src/components/common/Footer.js
+++ src/main/webapp/src/components/common/Footer.js
@@ -6,8 +6,7 @@ const Footer = () => {
   return (
     <div className="py-3 text-center">
       <CopyRightText>
-        &copy; 2023 <Link href="https://airnd.ru" target="_blank" rel="noopener noreferrer">AI RnD Lab</Link> | YaTeam
-        for Hackathon AI
+        &copy; 2023 <Link href="https://airnd.ru" target="_blank" rel="noopener noreferrer">AI RnD Lab</Link>
       </CopyRightText>
     </div>
   );
